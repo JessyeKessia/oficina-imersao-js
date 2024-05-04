ec7dd1fba90b6bc1583f11f6f57e0d09f470ec35
Jesye Pereira <jessyekessia16@gmail.com>
2024-05-04 17:13:05 -0300

    atualizado

--- src/entregasDesafio/Jessye-Pereira/valorProjeto.test.js
+++ src/entregasDesafio/Jessye-Pereira/valorProjeto.test.js
@@ -1,26 +1,20 @@
 const { calcularValorTotalProjeto } = require("../../dominio/calculadora/Projeto/valorProjeto.js")
-const moduloHoras = require("../../dominio/calculadora/Projeto/horasPorProjeto.js")
+
 const moduloPacote = require("../../dominio/calculadora/Projeto/pacote.js")
-jesk.mock("../../dominio/calculadora/Projeto/horasPorProjeto.js")
-jest.mock("../../dominio/calculadora/Projeto/valorProjeto.js") // simulação da função
+jest.mock("../../dominio/calculadora/Projeto/horasPorProjeto.js")
+//simulação da função
 describe("valorProjeto", () => {
     beforeAll(() => {
-        moduloPacote.calcularPacote.mockReturnValue('pacote_basico')
-        moduloHoras.calcularHorasDeProjeto.mockReturnValue(30);
+        moduloPacote.calcularPacote.mockReturnValue('pacote_intermediario')
 
-        test("cenario", () => {
+        test("Deve calcular o valor total do projeto", () => {
             const listaDeFuncionalidades = [
-                "setup", 
-                "responsividade", 
-                "construcao_1_pagina", 
-                "construcao_1_pagina",
-                "construcao_1_pagina", 
-                "formulario", 'ssr',
+                "setup"
             ];
-            const valorHora = 30
-            const resultado = calcularValorProjeto(listaDeFuncionalidades, valorHora)
+            const valorHora = 100
+            const resultado = calcularValorTotalProjeto(listaDeFuncionalidades, valorHora)
 
-            expect(resultado).toEqual(2750);
+            expect(resultado).toEqual(7200);
         })
     })
 })
